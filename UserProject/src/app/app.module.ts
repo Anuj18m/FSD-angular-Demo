a435fe87ba34e6948bc5d193338a85ef40398453
octalbits18@gmail.com <anuj17m@gmail.com>
2026-02-04 16:14:05 +0530

    feat: add core layout components including header, footer, and landing

--- UserProject/src/app/app.module.ts
+++ UserProject/src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { AuthModule } from './auth/auth.module';
+import { ProfilesModule } from './profiles/profiles.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    SharedModule,
+    DashboardModule,
+    AuthModule,
+    ProfilesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
